3289379c6e38fe8065926d0a2702e4915ecebc48
Mac <blacklantern78@yahoo.com>
2018-01-20 00:05:33 -0800

    Second Commit. Added Page controller. Added updated Maven Dependencies.

---
+++ onlineshopping/src/main/java/net/emf/onlineshopping/controller/PageController.java
@@ -0,0 +1,20 @@
+package net.emf.onlineshopping.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+	
+	
+	
+	@RequestMapping(value = {"/", "/home", "/index"})
+	public ModelAndView index() {
+		
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("What's Up!", "Welcome to the desert of the real MVC");
+		return mv;
+	}
+
+}
---
+++ onlineshopping/src/main/java/net/emf/onlineshopping/controller/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author Bruce
+ *
+ */
+package net.emf.onlineshopping.controller;
